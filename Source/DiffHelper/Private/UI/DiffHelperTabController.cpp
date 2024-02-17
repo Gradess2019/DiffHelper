5c5b0209167968e237ab474c04039c05953d8467
Gradess <trofimovjava@gmail.com>
2024-02-17 17:30:39 +0100

    [DIFF-5] Add controller

---
+++ Source/DiffHelper/Private/UI/DiffHelperTabController.cpp
@@ -0,0 +1,17 @@
+﻿// Copyright 2024 Gradess Games. All Rights Reserved.
+
+
+#include "UI/DiffHelperTabController.h"
+#include "UI/DiffHelperTabModel.h"
+
+void UDiffHelperTabController::Init()
+{
+	AddToRoot();
+	Model = NewObject<UDiffHelperTabModel>(this);
+}
+
+void UDiffHelperTabController::Deinit()
+{
+	RemoveFromRoot();
+	Model = nullptr;
+}
