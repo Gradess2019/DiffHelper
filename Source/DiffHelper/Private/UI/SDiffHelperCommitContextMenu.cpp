7928a3e69375ec3c7c4426ead24792aaece84d30
Gradess <trofimovjava@gmail.com>
2024-07-07 17:06:18 +0200

    [DIFF-21] Add context menu for commit panel

---
+++ Source/DiffHelper/Private/UI/SDiffHelperCommitContextMenu.cpp
@@ -0,0 +1,35 @@
+﻿// Copyright 2024 Gradess Games. All Rights Reserved.
+
+
+#include "UI/SDiffHelperCommitContextMenu.h"
+#include "DiffHelperCommands.h"
+#include "SlateOptMacros.h"
+
+BEGIN_SLATE_FUNCTION_BUILD_OPTIMIZATION
+
+void SDiffHelperCommitContextMenu::Construct(const FArguments& InArgs)
+{
+	if (!ensure(InArgs._CommandList.IsValid())) { return; }
+
+	FMenuBuilder MenuBuilder(true, InArgs._CommandList);
+
+	MenuBuilder.BeginSection("DiffHelper.CommitPanel.ContextMenu");
+	MenuBuilder.AddMenuEntry(FDiffHelperCommands::Get().DiffSelectedCommits);
+	MenuBuilder.AddMenuEntry(FDiffHelperCommands::Get().DiffSelectedCommitAgainstNext);
+	MenuBuilder.AddMenuEntry(FDiffHelperCommands::Get().DiffSelectedCommitAgainstPrevious);
+	MenuBuilder.AddMenuEntry(FDiffHelperCommands::Get().DiffSelectedCommitAgainstNewest);
+	MenuBuilder.AddMenuEntry(FDiffHelperCommands::Get().DiffSelectedCommitAgainstOldest);
+	MenuBuilder.EndSection();
+
+	ChildSlot
+	[
+		SNew(SBorder)
+		.BorderImage(FAppStyle::GetBrush("ToolPanel.GroupBorder"))
+		.Content()
+		[
+			MenuBuilder.MakeWidget()
+		]
+	];
+}
+
+END_SLATE_FUNCTION_BUILD_OPTIMIZATION
---
+++ Source/DiffHelper/Public/UI/SDiffHelperCommitContextMenu.h
@@ -0,0 +1,23 @@
+﻿// Copyright 2024 Gradess Games. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Widgets/SCompoundWidget.h"
+
+class UDiffHelperTabController;
+
+class DIFFHELPER_API SDiffHelperCommitContextMenu : public SCompoundWidget
+{
+public:
+	SLATE_BEGIN_ARGS(SDiffHelperCommitContextMenu)
+		{
+		}
+
+		SLATE_ARGUMENT(TSharedPtr<FUICommandList>, CommandList)
+
+	SLATE_END_ARGS()
+
+	/** Constructs this widget with InArgs */
+	void Construct(const FArguments& InArgs);
+};
