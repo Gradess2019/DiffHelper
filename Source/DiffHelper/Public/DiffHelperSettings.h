8fc30a6fbf2dacc65cafb4c42c735ef621a910ca
Gradess <trofimovjava@gmail.com>
2024-10-20 00:57:19 +0200

    [DIFF-59] Add dev mode and restructure settings

--- Source/DiffHelper/Public/DiffHelperSettings.h
+++ Source/DiffHelper/Public/DiffHelperSettings.h
@@ -13,6 +13,27 @@ class DIFFHELPER_API UDiffHelperSettings : public UObject
 	GENERATED_BODY()
 
 public:
+	/** If true, the plugin will try to find and reuse opened diff tab. If false, it will always open a new tab */
+	UPROPERTY(Config, EditAnywhere, Category = "General")
+	bool bReuseDiffTab = true;
+
+	/** if true the plugin will cache source and target branches and will put them into selectors on a first Diff Helper view */
+	UPROPERTY(Config, EditAnywhere, Category = "General")
+	bool bEnableCaching = true;
+	
+	/** Enables opening external diff for non-assets */
+	UPROPERTY(Config, EditAnywhere, Category = "General")
+	bool bEnableExternalDiff = false;
+
+	/** Command that will be used to open diff in external editor for non-assets. By default, it's set to Visual Studio Code
+	* {0} - source file path
+	* {1} - target file path
+	* {2} - source file revision
+	* {3} - target file revision
+	*/
+	UPROPERTY(Config, EditAnywhere, Category = "General", meta = (EditCondition = "bEnableExternalDiff"))
+	FString ExternalDiffCommand = TEXT("code --diff {0} {1}");
+	
 	/** Files with these statuses couldn't be used for diff */
 	UPROPERTY(Config, EditAnywhere, Category = "General")
 	TArray<EDiffHelperFileStatus> StatusBlacklist = {
@@ -20,89 +41,6 @@ public:
 		EDiffHelperFileStatus::Deleted,
 		EDiffHelperFileStatus::Unmerged,
 	};
-
-	/** If true, the plugin will try to find and reuse opened diff tab. If false, it will always open a new tab */
-	UPROPERTY(Config, EditAnywhere, Category = "General")
-	bool bReuseDiffTab = true;
-
-	/** Enables opening external diff for non-assets */
-	UPROPERTY(Config, EditAnywhere, Category = "Diff")
-	bool bEnableExternalDiff = false;
-
-	/** Command that will be used to open diff in external editor for non-assets. By default, it's set to Visual Studio Code
-	 * {0} - source file path
-	 * {1} - target file path
-	 * {2} - source file revision
-	 * {3} - target file revision
-	 */
-	UPROPERTY(Config, EditAnywhere, Category = "Diff", meta = (EditCondition = "bEnableExternalDiff"))
-	FString ExternalDiffCommand = TEXT("code --diff {0} {1}");
-
-	/** if true the plugin will cache source and target branches and will put them into selectors on a first Diff Helper view */
-	UPROPERTY(Config, EditAnywhere, Category = "Caching")
-	bool bEnableCaching = true;
-
-#pragma region Git
-	UPROPERTY(Config, EditAnywhere, Category = "Git|Branch Parsing")
-	FString BranchParserPattern = TEXT("(?:\n*\\s\\s|(?:\\*\\s))\\(*(.+?)\\s+(?:detached at \\w+\\)\\s)*(\\w+)");
-
-	UPROPERTY(Config, EditAnywhere, Category = "Git|Branch Parsing")
-	int32 BranchNameGroup = 1;
-
-	UPROPERTY(Config, EditAnywhere, Category = "Git|Branch Parsing")
-	int32 BranchRevisionGroup = 2;
-
-	UPROPERTY(Config, EditAnywhere, Category = "Git|Commit Parsing")
-	FString CommitBlockPattern = TEXT("<Hash[\\s\\S]+?(?=<Hash)|<Hash[\\s\\S]+(?=$)");
-
-	UPROPERTY(Config, EditAnywhere, Category = "Git|Commit Parsing")
-	FString CommitDataPattern = TEXT("<Hash:(.+?)> <Message:(.+?)> <Author:(.+?)> <Date:(.+?)>\n([\\s\\S]*?)(?=(?:<Hash:\\w+>|$))\n$");
-
-	UPROPERTY(Config, EditAnywhere, Category = "Git|Commit Parsing")
-	FString SingleCommitPattern = TEXT("<Hash:(.+?)> <Message:(.+?)> <Author:(.+?)> <Date:(.+?)>");
-
-	UPROPERTY(Config, EditAnywhere, Category = "Git|Commit Parsing")
-	int32 HashGroup = 1;
-
-	UPROPERTY(Config, EditAnywhere, Category = "Git|Commit Parsing")
-	int32 MessageGroup = 2;
-
-	UPROPERTY(Config, EditAnywhere, Category = "Git|Commit Parsing")
-	int32 AuthorGroup = 3;
-
-	UPROPERTY(Config, EditAnywhere, Category = "Git|Commit Parsing")
-	int32 DateGroup = 4;
-
-	UPROPERTY(Config, EditAnywhere, Category = "Git|Commit Parsing")
-	int32 ChangedFilesGroup = 5;
-
-	UPROPERTY(Config, EditAnywhere, Category = "Git|Date Parsing")
-	FString DatePattern = TEXT("(\\d+)/(\\d+)/(\\d+) (\\d+):(\\d+)");
-
-	UPROPERTY(Config, EditAnywhere, Category = "Git|Date Parsing")
-	int32 DayGroup = 1;
-
-	UPROPERTY(Config, EditAnywhere, Category = "Git|Date Parsing")
-	int32 MonthGroup = 2;
-
-	UPROPERTY(Config, EditAnywhere, Category = "Git|Date Parsing")
-	int32 YearGroup = 3;
-
-	UPROPERTY(Config, EditAnywhere, Category = "Git|Date Parsing")
-	int32 HourGroup = 4;
-
-	UPROPERTY(Config, EditAnywhere, Category = "Git|Date Parsing")
-	int32 MinuteGroup = 5;
-
-	UPROPERTY(Config, EditAnywhere, Category = "Git|Changes Parsing")
-	FString ChangedFilePattern = TEXT("(.).*\t(.*)(?:[\\s\n]|$)");
-
-	UPROPERTY(Config, EditAnywhere, Category = "Git|Changes Parsing")
-	int32 ChangedFileStatusGroup = 1;
-
-	UPROPERTY(Config, EditAnywhere, Category = "Git|Changes Parsing")
-	int32 ChangedFilePathGroup = 2;
-#pragma endregion Git
 
 	UPROPERTY(Config, EditAnywhere, Category = "Appearance|Revision Picker")
 	float PickerPanelWidth = 350.f;
@@ -131,10 +69,11 @@ public:
 	UPROPERTY(Config, EditAnywhere, Category = "Appearance|Diff Viewer", meta = (ClampMin = "0.05", ClampMax = "0.95", UIMin = "0.05", UIMax = "0.95", Delta = "0.01"))
 	float DateColumnRatio = 0.2f;
 
-	// Color of the hint that is located on the right side of the item name in the Diff List, that displays files / tree structure
+	/** Color of the hint that is located on the right side of the item name in the Diff List, that displays files / tree structure */
 	UPROPERTY(Config, EditAnywhere, Category = "Appearance|Diff Viewer")
 	FLinearColor ItemHintColor = FLinearColor(0.380, 0.427, 0.443, 0.5);
-	
+
+	/** Color of file in the List / Tree in Diff Viewer */
 	UPROPERTY(Config, EditAnywhere, Category = "Appearance|Diff Viewer")
 	TMap<EDiffHelperFileStatus, FLinearColor> StatusColors = {
 		{EDiffHelperFileStatus::None, FLinearColor(1.f, 1.f, 1.f)},
@@ -154,6 +93,72 @@ public:
 
 	UPROPERTY(Config, EditAnywhere, Category = "Misc")
 	FString UnrealDocURL = TEXT("https://dev.epicgames.com/documentation/en-us/unreal-engine/collaboration-and-version-control-in-unreal-engine");
+
+#pragma region Git
+	/** Dev mode activates additional settings for Git parsing. DO NOT USE UNTIL YOU KNOW WHAT YOU'RE DOING. */
+	UPROPERTY(Config, EditAnywhere, Category = "Git")
+	bool bDevMode = false;
+	
+	UPROPERTY(Config, EditAnywhere, Category = "Git|Branch Parsing", meta = (EditCondition = "bDevMode"))
+	FString BranchParserPattern = TEXT("(?:\n*\\s\\s|(?:\\*\\s))\\(*(.+?)\\s+(?:detached at \\w+\\)\\s)*(\\w+)");
+
+	UPROPERTY(Config, EditAnywhere, Category = "Git|Branch Parsing", meta = (EditCondition = "bDevMode"))
+	int32 BranchNameGroup = 1;
+
+	UPROPERTY(Config, EditAnywhere, Category = "Git|Branch Parsing", meta = (EditCondition = "bDevMode"))
+	int32 BranchRevisionGroup = 2;
+
+	UPROPERTY(Config, EditAnywhere, Category = "Git|Commit Parsing", meta = (EditCondition = "bDevMode"))
+	FString CommitBlockPattern = TEXT("<Hash[\\s\\S]+?(?=<Hash)|<Hash[\\s\\S]+(?=$)");
+
+	UPROPERTY(Config, EditAnywhere, Category = "Git|Commit Parsing", meta = (EditCondition = "bDevMode"))
+	FString CommitDataPattern = TEXT("<Hash:(.+?)> <Message:(.+?)> <Author:(.+?)> <Date:(.+?)>\n([\\s\\S]*?)(?=(?:<Hash:\\w+>|$))\n$");
+
+	UPROPERTY(Config, EditAnywhere, Category = "Git|Commit Parsing", meta = (EditCondition = "bDevMode"))
+	FString SingleCommitPattern = TEXT("<Hash:(.+?)> <Message:(.+?)> <Author:(.+?)> <Date:(.+?)>");
+
+	UPROPERTY(Config, EditAnywhere, Category = "Git|Commit Parsing", meta = (EditCondition = "bDevMode"))
+	int32 HashGroup = 1;
+
+	UPROPERTY(Config, EditAnywhere, Category = "Git|Commit Parsing", meta = (EditCondition = "bDevMode"))
+	int32 MessageGroup = 2;
+
+	UPROPERTY(Config, EditAnywhere, Category = "Git|Commit Parsing", meta = (EditCondition = "bDevMode"))
+	int32 AuthorGroup = 3;
+
+	UPROPERTY(Config, EditAnywhere, Category = "Git|Commit Parsing", meta = (EditCondition = "bDevMode"))
+	int32 DateGroup = 4;
+
+	UPROPERTY(Config, EditAnywhere, Category = "Git|Commit Parsing", meta = (EditCondition = "bDevMode"))
+	int32 ChangedFilesGroup = 5;
+
+	UPROPERTY(Config, EditAnywhere, Category = "Git|Date Parsing", meta = (EditCondition = "bDevMode"))
+	FString DatePattern = TEXT("(\\d+)/(\\d+)/(\\d+) (\\d+):(\\d+)");
+
+	UPROPERTY(Config, EditAnywhere, Category = "Git|Date Parsing", meta = (EditCondition = "bDevMode"))
+	int32 DayGroup = 1;
+
+	UPROPERTY(Config, EditAnywhere, Category = "Git|Date Parsing", meta = (EditCondition = "bDevMode"))
+	int32 MonthGroup = 2;
+
+	UPROPERTY(Config, EditAnywhere, Category = "Git|Date Parsing", meta = (EditCondition = "bDevMode"))
+	int32 YearGroup = 3;
+
+	UPROPERTY(Config, EditAnywhere, Category = "Git|Date Parsing", meta = (EditCondition = "bDevMode"))
+	int32 HourGroup = 4;
+
+	UPROPERTY(Config, EditAnywhere, Category = "Git|Date Parsing", meta = (EditCondition = "bDevMode"))
+	int32 MinuteGroup = 5;
+
+	UPROPERTY(Config, EditAnywhere, Category = "Git|Changes Parsing", meta = (EditCondition = "bDevMode"))
+	FString ChangedFilePattern = TEXT("(.).*\t(.*)(?:[\\s\n]|$)");
+
+	UPROPERTY(Config, EditAnywhere, Category = "Git|Changes Parsing", meta = (EditCondition = "bDevMode"))
+	int32 ChangedFileStatusGroup = 1;
+
+	UPROPERTY(Config, EditAnywhere, Category = "Git|Changes Parsing", meta = (EditCondition = "bDevMode"))
+	int32 ChangedFilePathGroup = 2;
+#pragma endregion Git
 
 public:
 	static bool IsCachingEnabled() { return GetDefault<UDiffHelperSettings>()->bEnableCaching; }
