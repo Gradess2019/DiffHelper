6428d0b8069c69a5b373b28d5522e43874eaec81
Gradess <trofimovjava@gmail.com>
2024-10-19 23:55:39 +0200

    [DIFF-59] Structure categories

--- Source/DiffHelper/Public/DiffHelperSettings.h
+++ Source/DiffHelper/Public/DiffHelperSettings.h
@@ -43,64 +43,64 @@ public:
 	bool bEnableCaching = true;
 
 #pragma region Git
-	UPROPERTY(Config, EditAnywhere, Category = "Git")
+	UPROPERTY(Config, EditAnywhere, Category = "Git|Branch Parsing")
 	FString BranchParserPattern = TEXT("(?:\n*\\s\\s|(?:\\*\\s))\\(*(.+?)\\s+(?:detached at \\w+\\)\\s)*(\\w+)");
 
-	UPROPERTY(Config, EditAnywhere, Category = "Git")
+	UPROPERTY(Config, EditAnywhere, Category = "Git|Branch Parsing")
 	int32 BranchNameGroup = 1;
 
-	UPROPERTY(Config, EditAnywhere, Category = "Git")
+	UPROPERTY(Config, EditAnywhere, Category = "Git|Branch Parsing")
 	int32 BranchRevisionGroup = 2;
 
-	UPROPERTY(Config, EditAnywhere, Category = "Git")
+	UPROPERTY(Config, EditAnywhere, Category = "Git|Commit Parsing")
 	FString CommitBlockPattern = TEXT("<Hash[\\s\\S]+?(?=<Hash)|<Hash[\\s\\S]+(?=$)");
 
-	UPROPERTY(Config, EditAnywhere, Category = "Git")
+	UPROPERTY(Config, EditAnywhere, Category = "Git|Commit Parsing")
 	FString CommitDataPattern = TEXT("<Hash:(.+?)> <Message:(.+?)> <Author:(.+?)> <Date:(.+?)>\n([\\s\\S]*?)(?=(?:<Hash:\\w+>|$))\n$");
 
-	UPROPERTY(Config, EditAnywhere, Category = "Git")
+	UPROPERTY(Config, EditAnywhere, Category = "Git|Commit Parsing")
 	FString SingleCommitPattern = TEXT("<Hash:(.+?)> <Message:(.+?)> <Author:(.+?)> <Date:(.+?)>");
 
-	UPROPERTY(Config, EditAnywhere, Category = "Git")
+	UPROPERTY(Config, EditAnywhere, Category = "Git|Commit Parsing")
 	int32 HashGroup = 1;
 
-	UPROPERTY(Config, EditAnywhere, Category = "Git")
+	UPROPERTY(Config, EditAnywhere, Category = "Git|Commit Parsing")
 	int32 MessageGroup = 2;
 
-	UPROPERTY(Config, EditAnywhere, Category = "Git")
+	UPROPERTY(Config, EditAnywhere, Category = "Git|Commit Parsing")
 	int32 AuthorGroup = 3;
 
-	UPROPERTY(Config, EditAnywhere, Category = "Git")
+	UPROPERTY(Config, EditAnywhere, Category = "Git|Commit Parsing")
 	int32 DateGroup = 4;
 
-	UPROPERTY(Config, EditAnywhere, Category = "Git")
+	UPROPERTY(Config, EditAnywhere, Category = "Git|Commit Parsing")
 	int32 ChangedFilesGroup = 5;
 
-	UPROPERTY(Config, EditAnywhere, Category = "Git")
+	UPROPERTY(Config, EditAnywhere, Category = "Git|Date Parsing")
 	FString DatePattern = TEXT("(\\d+)/(\\d+)/(\\d+) (\\d+):(\\d+)");
 
-	UPROPERTY(Config, EditAnywhere, Category = "Git")
+	UPROPERTY(Config, EditAnywhere, Category = "Git|Date Parsing")
 	int32 DayGroup = 1;
 
-	UPROPERTY(Config, EditAnywhere, Category = "Git")
+	UPROPERTY(Config, EditAnywhere, Category = "Git|Date Parsing")
 	int32 MonthGroup = 2;
 
-	UPROPERTY(Config, EditAnywhere, Category = "Git")
+	UPROPERTY(Config, EditAnywhere, Category = "Git|Date Parsing")
 	int32 YearGroup = 3;
 
-	UPROPERTY(Config, EditAnywhere, Category = "Git")
+	UPROPERTY(Config, EditAnywhere, Category = "Git|Date Parsing")
 	int32 HourGroup = 4;
 
-	UPROPERTY(Config, EditAnywhere, Category = "Git")
+	UPROPERTY(Config, EditAnywhere, Category = "Git|Date Parsing")
 	int32 MinuteGroup = 5;
 
-	UPROPERTY(Config, EditAnywhere, Category = "Git")
+	UPROPERTY(Config, EditAnywhere, Category = "Git|Changes Parsing")
 	FString ChangedFilePattern = TEXT("(.).*\t(.*)(?:[\\s\n]|$)");
 
-	UPROPERTY(Config, EditAnywhere, Category = "Git")
+	UPROPERTY(Config, EditAnywhere, Category = "Git|Changes Parsing")
 	int32 ChangedFileStatusGroup = 1;
 
-	UPROPERTY(Config, EditAnywhere, Category = "Git")
+	UPROPERTY(Config, EditAnywhere, Category = "Git|Changes Parsing")
 	int32 ChangedFilePathGroup = 2;
 #pragma endregion Git
 
@@ -114,10 +114,10 @@ public:
 	float CommitPanelMinSize = 200.f;
 	
 	UPROPERTY(Config, EditAnywhere, Category = "Appearance|Diff Viewer", meta = (ClampMin = "0.1", ClampMax = "0.9", UIMin = "0.1", UIMax = "0.9", Delta = "0.05"))
-	float DiffPanelRatio = 0.6f;
+	float DiffPanelRatio = 0.5f;
 
 	UPROPERTY(Config, EditAnywhere, Category = "Appearance|Diff Viewer", meta = (ClampMin = "0.1", ClampMax = "0.9", UIMin = "0.1", UIMax = "0.9", Delta = "0.05"))
-	float CommitPanelRatio = 0.4f;
+	float CommitPanelRatio = 0.5f;
 
 	UPROPERTY(Config, EditAnywhere, Category = "Appearance|Diff Viewer", meta = (ClampMin = "0.05", ClampMax = "0.95", UIMin = "0.05", UIMax = "0.95", Delta = "0.01"))
 	float HashColumnRatio = 0.15f;
@@ -130,8 +130,12 @@ public:
 
 	UPROPERTY(Config, EditAnywhere, Category = "Appearance|Diff Viewer", meta = (ClampMin = "0.05", ClampMax = "0.95", UIMin = "0.05", UIMax = "0.95", Delta = "0.01"))
 	float DateColumnRatio = 0.2f;
+
+	// Color of the hint that is located on the right side of the item name in the Diff List, that displays files / tree structure
+	UPROPERTY(Config, EditAnywhere, Category = "Appearance|Diff Viewer")
+	FLinearColor ItemHintColor = FLinearColor(0.380, 0.427, 0.443, 0.5);
 	
-	UPROPERTY(Config, EditAnywhere, Category = "Appearance")
+	UPROPERTY(Config, EditAnywhere, Category = "Appearance|Diff Viewer")
 	TMap<EDiffHelperFileStatus, FLinearColor> StatusColors = {
 		{EDiffHelperFileStatus::None, FLinearColor(1.f, 1.f, 1.f)},
 		{EDiffHelperFileStatus::Added, FLinearColor(0.384f, 0.8f, 0.333f)},
@@ -141,10 +145,6 @@ public:
 		{EDiffHelperFileStatus::Copied, FLinearColor(0.039f, 0.467f, 0.0f)},
 		{EDiffHelperFileStatus::Unmerged, FLinearColor(0.835f, 0.459f, 0.424f)},
 	};
-
-	// Color of the hint that is located on the right side of the item name in the Diff List, that displays files / tree structure
-	UPROPERTY(Config, EditAnywhere, Category = "Appearance")
-	FLinearColor ItemHintColor = FLinearColor(0.380, 0.427, 0.443, 0.5);
 
 	UPROPERTY(Config, EditAnywhere, Category = "Notification")
 	bool bUseSuccessFailIcons = true;
